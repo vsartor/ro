257ed47fbc775a53e1792447a5d2274aa6ae21cc
Victhor Sartório <victhor@vsartor.com>
2020-02-25 23:33:05 -0300

    Connect related comments with empty comments

--- donna/donna.go
+++ donna/donna.go
@@ -10,13 +10,13 @@ import "ro/weems"
 // Donna Paulsen handles command line arguments for Ro.
 
 // It handles two different types of arguments: options and arguments.
-
+//
 // Options are optional command line arguments with default values and
 // that starts with "-", either containing a parameter if integer or
 // string, or simply being a flag.
-
+//
 // Arguments are command line arguments that do not have a preceding dash.
-
+//
 // There are two kinds of options, global options, handled at startup and
 // consumed by ro's entry point, and command specific options which are
 // handled by the executing code.
