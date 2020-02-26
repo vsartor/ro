0479ab69e50181377dea8e780932c6c5d83498fa
Victhor Sartório <victhor@vsartor.com>
2020-02-26 00:51:35 -0300

    Rewrite Donna's explanation

--- donna/donna.go
+++ donna/donna.go
@@ -8,18 +8,27 @@ package donna
 import "ro/weems"
 
 // Donna handles command line arguments for Ro.
-
-// It handles two different types of arguments: options and arguments.
 //
-// Options are optional command line arguments with default values and
-// that starts with "-", either containing a parameter if integer or
-// string, or simply being a flag.
+// It handles three different types of command line parameters: flags,
+// options and arguments.
 //
-// Arguments are command line arguments that do not have a preceding dash.
+// Flags start with a "--" and do not take arguments, their presence/absence
+// indicates their value. Options start with a single dash "-" and take in a
+// following value, associated to that option. Arguments are regular names.
 //
-// There are two kinds of options, global options, handled at startup and
-// consumed by ro's entry point, and command specific options which are
-// handled by the executing code.
+// Options and flags can either be global, relating to Ro's own operations,
+// preceding all arguments, or command specific, following all arguments.
+// To illustrate, this is Ro's usage map:
+//     ro [global flags/options] <argument chain> [command flags/options]
+//
+// In case this invocation rule is violated Donna will log a critical message.
+//
+// Flags and options are validated, with global ones being validated at
+// entry point, and with command ones being validated at the command routine's
+// entry point. Their values are consumed at request.
+//
+// Arguments are consumed one by one and validation happens online as each
+// value is consumed before dispatching to another routine.
 
 var logger weems.Logger
 
